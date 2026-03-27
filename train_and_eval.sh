922e91665fda85fa7948e5532aafda26fecbe709
--global <--global>
2026-03-27 03:02:09 +0000

    finish opd run

--- train_and_eval.sh
+++ train_and_eval.sh
@@ -1,7 +1,7 @@
 #python evals/evaluate.py Qwen/Qwen3-4B --max-tokens 3072 --batch-size 100 #--max-samples 200
 #python evals/evaluate.py Qwen/Qwen3-30B-A3B --max-tokens 3072 --batch-size 100 #--max-samples 200
 
-accelerate launch rl/grpo_run.py
+#accelerate launch rl/grpo_run.py
 python evals/evaluate.py ViratChauhan/Qwen3-4B-GRPO --max-tokens 3072 --batch-size 100 #--max-samples 200
 bash scripts/run_opd.sh
 python evals/evaluate.py ViratChauhan/Qwen3-4B-OPD --max-tokens 3072 --batch-size 100 #--max-samples 200
